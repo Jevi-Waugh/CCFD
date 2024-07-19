2b03da1b551008af5b0f4c1d471e4565ebe7baad
Jevi Waugh <codeprogramming99@gmail.com>
2024-07-19 16:08:18 +1000

    wrapper func for argsort_func

--- argsort.py
+++ argsort.py
@@ -1,15 +1,24 @@
+import numpy as np
 
 
-arr = [50,4,30,32,1]
-
-def func(index : int):
-    return arr[index]
+def key_func_warpper(arr: list):
+    # returns value of the array based off index param
+    def key_func(index : int):
+        return arr[index]
+    return key_func
 
 def argsort_lambda(arr: list):
     return sorted(range(len(arr)), key=lambda x: arr[x])
+
+def argsort_slice(arr: list, slice: int):
+    return sorted(range(len(arr)), key=lambda x: arr[x])[:slice]
 
 def argsort_get_item(arr: list):
     return sorted(range(len(arr)), key=arr.__getitem__)
 
 def argsort_func(arr: list):
-    return sorted(range(len(arr)), key=func)
+    key_func = key_func_warpper(arr)
+    return sorted(range(len(arr)), key=key_func)
+
+def argsort_numpy(arr, slice):
+    return np.argsort(arr) if slice is None else np.argsort(arr)[:slice]
